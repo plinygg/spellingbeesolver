6953f9efce75a8f04e1de847ae4cab86d9f14590
atchang <aidan.chang@outlook.com>
2024-05-09 10:06:09 -0500

    started 1 - implementation

--- printer.py
+++ printer.py
@@ -8,6 +8,9 @@ for word in wor:
     if not len(word) <= 3:
         words.append(word)
 
+alpha = "abcdefghijklmnopqrstuvwxyz"
+alphabet = [letter for letter in alpha]
+
 while True:
     inputs = input('Input middle letter:\n')
     if len(inputs) > 1:
@@ -18,18 +21,15 @@ while True:
             if inputs.lower() in word:
                 temp.append(word)
         words = sorted(temp, key=len)[::-1]
-        input2 = input('Input the surrounding letters separated by a space.\n')
-        if not (',' in input2):
-            temp2 = []
-            gene = words
-            for i in range(len(words)):
-                for letter in input2.lower().replace(' ', ''):
-                    gene[i].replace(letter, '')
-                if gene[i] == '':
-                    temp2.append(words[i])
-            words = temp2
-            print('Word list complete. Words:')
-            print(words)
-        else:
-            print('No commas.')
-
+        counter = 0
+        while counter <= 5:
+            input2 = input('Input the surrounding letters one by one.\n')
+            alphabet.remove(input2.lower())
+            counter += 1
+        print(alphabet)
+        for word in words:
+            for letter_left in alphabet:
+                if letter_left in word:
+                    words.remove(word)
+                    #print(word)
+        print(words)
